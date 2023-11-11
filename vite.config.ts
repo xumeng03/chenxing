c82dd5288064e2a9e0fb66da104e7f020ce7c9bc
xumeng03 <xumeng03@qq.com>
2023-11-11 23:10:13 +0800

    build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,32 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import path from "path";
 import vueJsxPlugin from "@vitejs/plugin-vue-jsx";
+import dtsPlugin from "vite-plugin-dts";
+import path from "path";
 
 export default defineConfig({
-  plugins: [vue(), vueJsxPlugin()],
+  plugins: [vue(), vueJsxPlugin(), dtsPlugin()],
   resolve: {
     // 配置路径别名
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ["vue"],
+      output: [
+        {
+          entryFileNames: "[name].mjs",
+          preserveModules: true,
+        },
+      ],
+    },
+    minify: false,
+    lib: {
+      entry: "./index.ts",
+      name: "chenxing",
+      fileName: "chenxing",
     },
   },
   server: {
